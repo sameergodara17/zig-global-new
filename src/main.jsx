70b83823e7cae533c17804ceaf60fda0a0a4b1c5
sameergodara17 <sameergodara17@gmail.com>
2026-02-25 12:01:52 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Website from "./Website";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Website />
-  </React.StrictMode>
-);
